54adf0955b45eb0cf799528417888dd2dbfaa0cb
Christian Paling <christian.paling@googlemail.com>
2017-02-11 16:45:07 +0100

    Add artisan command to enqueue reminders

---
+++ app/Console/Commands/EnqueueReminderMails.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use \App\Reminder;
+use Carbon\Carbon;
+
+class EnqueueReminderMails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'schedule:reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Enqueue all reminders that are due';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $reminders = Reminder::where('reminder_date', '<=', Carbon::now())->get();
+
+        foreach ($reminders as $reminder) {
+            // enqueue job
+        }
+    }
+}

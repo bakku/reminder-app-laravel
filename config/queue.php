4edfec9b88e409a61887ab7dd720822aa2844194
Christian Paling <christian.paling@googlemail.com>
2017-02-11 17:04:30 +0100

    Adjust queue configuration and jobs table

--- config/queue.php
+++ config/queue.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_DRIVER', 'sync'),
+    'default' => env('QUEUE_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     */
 
     'failed' => [
-        'database' => env('DB_CONNECTION', 'mysql'),
+        'database' => env('DB_CONNECTION', 'pgsql'),
         'table' => 'failed_jobs',
     ],
 
